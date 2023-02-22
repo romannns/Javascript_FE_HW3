fe534cbac9bf6f3b4c6156d122f6d8aba1b6898d
romannns <122488529+romannns@users.noreply.github.com>
2023-02-22 18:40:33 +0200

    Add files via upload

---
+++ HW_FE_Javascript_3/script/script.js
@@ -0,0 +1,25 @@
+const button1 = document.querySelector('.button_1');
+const button2 = document.querySelector('.button_2');
+const button3 = document.querySelector('.button_3');
+const button4 = document.querySelector('.button_4');
+const button5 = document.querySelector('.button_5');
+
+button1.addEventListener('click', function(){
+    alert('#fed330');
+});
+
+button2.addEventListener('click', function(){
+    alert('#26de81');
+});
+
+button3.addEventListener('click', function(){
+    alert('#eb3b5a');
+});
+
+button4.addEventListener('click', function(){
+    alert('#4b7bec');
+});
+
+button5.addEventListener('click', function(){
+    alert('#4b6584');
+});
